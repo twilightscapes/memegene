ea8d77f6dd0d5208e4b2419588de49ae319d5647
Todd <ihatespam@gmail.com>
2024-03-01 20:11:31 -0600

    Update update-theme.sh

--- update-theme.sh
+++ update-theme.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
 # Set the URL of your central repository
-# THEME_REPO_URL="https://github.com/twilightscapes/pirateplus"
+THEME_REPO_URL="https://github.com/twilightscapes/pirateplus"
 
-# # Set the branch or tag you want to pull updates from
-# BRANCH_OR_TAG="main"
+# Set the branch or tag you want to pull updates from
+BRANCH_OR_TAG="main"
 
-# # Check if src directory exists and move it to user_src_backup if it does
-# if [ -d "src" ]; then
-#     mv src user_src_backup
-# else
-#     echo "src directory not found, skipping backup"
-# fi
+# Backup user changes
+mv src user_src_backup
 
-# # Clone the central repository
-# git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
+# Clone the central repository
+git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 
-# # Replace the src folder, excluding index.js files in pages directory
-# find tmp_theme/src -mindepth 1 -type f -not -path "*/pages/index.js" -exec rsync -R {} ./src \;
+# Replace the src folder, excluding index.js files in pages directory
+find tmp_theme/src -type f -not -path "*/pages/index.js" -exec cp --parents {} ./src \;
 
-# # Replace the gatsby-config.js file
-# cp tmp_theme/gatsby-config.js .
+# Replace the gatsby-config.js file
+cp tmp_theme/gatsby-config.js .
 
-# # Replace the gatsby-node.js file
-# cp tmp_theme/gatsby-node.js .
+# Replace the gatsby-node.js file
+cp tmp_theme/gatsby-node.js .
 
-# # Replace the netlify.toml file
-# cp tmp_theme/netlify.toml .
+# Replace the netlify.toml file
+cp tmp_theme/netlify.toml .
 
-# # Update the admin/config.yml file
-# cp tmp_theme/static/admin/config.yml static/admin/
+# Update the admin/config.yml file
+cp tmp_theme/static/admin/config.yml static/admin/
 
-# # Copy the package.json file
-# cp tmp_theme/package.json .
+# Copy the package.json file
+cp tmp_theme/package.json .
 
-# # Move everything from tmp_theme/src to src/
-# mv tmp_theme/src/* src/
+# Clean up
+rm -rf tmp_theme
 
-# # Clean up
-# rm -rf tmp_theme
-
-# echo "PIRATE Plus updated successfully!"
+echo "PIRATE Plus updated successfully!"
